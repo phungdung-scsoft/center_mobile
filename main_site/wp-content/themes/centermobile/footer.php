2f6bc73cac8f83f7ccdb677cb776c25686554ff8
Mouri <mouri@zeromobile.co.jp>
2022-01-12 18:58:58 +0900

    mod: 店舗一覧ページの導線を非表示の状態でコミット

--- main_site/wp-content/themes/centermobile/footer.php
+++ main_site/wp-content/themes/centermobile/footer.php
@@ -30,7 +30,7 @@
                     <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>fc/">FC・OEMをご検討の方</a></li>
                     <!-- <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">広告出稿をご検討の方</a></li> -->
                     <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>media-archive/">トピックス</a></li>
-                    <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>store/">店舗一覧</a></li>
+                    <!--<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>store/">店舗一覧</a></li>-->
                     <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>policy/">プライバシーポリシー</a></li>
                     <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>contact-top/">お問い合わせ</a></li>
                   </div>
