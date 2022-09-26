dbd2ea80ff8b4238d709c89ff78a78cdc5f815ed
Mouri <mouri@zeromobile.co.jp>
2022-09-26 10:17:58 +0900

    mod: トップスライドのサムネイル数を変数化

--- main_site/wp-content/themes/centermobile/assets/js/script.js
+++ main_site/wp-content/themes/centermobile/assets/js/script.js
@@ -1,61 +1,59 @@
 //modal
 $(function(){
-	　　　$('.menu-trigger').on("click", function(){
-	　　　　if ($('.menu-trigger').hasClass('active')) {
-	　　　　　$('.menu-trigger').removeClass('active');
-	　　　　} else {
-	　　　　　$('.menu-trigger').addClass('active');
-	　　　　}
-	　　　});
-	　　});
+	$('.menu-trigger').on("click", function(){
+		if ($('.menu-trigger').hasClass('active')) {
+			$('.menu-trigger').removeClass('active');
+		} else {
+			$('.menu-trigger').addClass('active');
+		}
+	});
 
+	$('.menu-trigger').on("click", function(){
+		if ($('.gnav').hasClass('active')) {
+			$('.gnav').removeClass('active');
+		} else {
+			$('.gnav').addClass('active');
+		}
+	});
 
-$(function(){
-　　　$('.menu-trigger').on("click", function(){
-　　　　if ($('.gnav').hasClass('active')) {
-　　　　　$('.gnav').removeClass('active');
-　　　　} else {
-　　　　　$('.gnav').addClass('active');
-　　　　}
-　　　});
-　　});
-
-$(function(){
-　　　$('.menu-trigger').on("click", function(){
-　　　　if ($('body').hasClass('open')) {
-　　　　　$('body').removeClass('open');
-　　　　} else {
-　　　　　$('body').addClass('open');
-　　　　}
-　　　});
-　　});
+	$('.menu-trigger').on("click", function(){
+		if ($('body').hasClass('open')) {
+			$('body').removeClass('open');
+		} else {
+			$('body').addClass('open');
+		}
+	});
+});
 
 
 //slider
-$('.slider').slick({
-	autoplay: true,
-	arrows: false,
-	asNavFor: ".thumb",
-	slidesToShow: 1,
-	centerMode: true,
-	centerPadding: "25%",
-	waitForAnimate: false,
-	responsive: [
-		{
-		  breakpoint: 1921,
-		  settings: {
-				slidesToShow: 1,
-				centerMode: false,
+$(function(){
+	$('.slider').slick({
+		autoplay: true,
+		arrows: false,
+		asNavFor: ".thumb",
+		slidesToShow: 1,
+		centerMode: true,
+		centerPadding: "25%",
+		waitForAnimate: false,
+		responsive: [
+			{
+			  breakpoint: 1921,
+			  settings: {
+					slidesToShow: 1,
+					centerMode: false,
+				}
 			}
-		}
-	]
-});
+		]
+	});
 
-$('.thumb').slick({
-	asNavFor: '.slider', // スライダを他のスライダのナビゲーションに設定する（class名またはID名）
-	focusOnSelect: true, // クリックでのスライド切り替えを有効にするか
-	slidesToShow: 5, // 表示するスライド数を設定
-	waitForAnimate: false,
+	var slidesToshow_leng = $('.thumb li').length;
+	$('.thumb').slick({
+		asNavFor: '.slider', // スライダを他のスライダのナビゲーションに設定する（class名またはID名）
+		focusOnSelect: true, // クリックでのスライド切り替えを有効にするか
+		slidesToShow: slidesToshow_leng, // 表示するスライド数を設定
+		waitForAnimate: false,
+	});
 });
 
 
