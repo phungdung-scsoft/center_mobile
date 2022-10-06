f528d6d584ea3bbd251202e0a1f00f70ab1a8643
Mouri <mouri@zeromobile.co.jp>
2022-10-06 17:42:09 +0900

    add: 電話リレーサービス料ページ追加

---
+++ main_site/wp-content/themes/centermobile/page-telephonerelay.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <?php get_header(); ?>
+</head>
+  <body>
+
+    <!--ここから共通部分-->
+<?php get_template_part('inc/header'); ?>
+<!--ここまで共通部分-->
+      <!-- ここからコンテンツ -->
+    <div class="container">
+      <div class="img_telephonerelay top_image2">
+        <div class="top_tlt">
+        <div class="top_tlt_inner">
+        <h1 class="telephonerelay_h1">電話リレーサービス料</h1>
+        <p>About Telephonerelay</p>
+        </div>
+        </div>
+      </div>
+
+    <section class="content">
+      <div class="wrapper">
+        <h2>電話リレーサービス料について</h2>
+        <p>電話リレーサービスとは、聴覚や発話に困難のある方とそうでない方との会話を通訳オペレータが手話・文字などで通訳することにより電話で双方向につなぐサービスです。</p>
+      </div>
+    </section>
+
+    <section class="content">
+      <div class="wrapper">
+        <h2>番号単価（※1）</h2>
+        <p>2022年度の請求は以下の表の通りです。（※2）</p>
+
+        <div class="claim">
+          <dl>
+            <dt>4月</dt>
+            <dd>1.1円</dd>
+          </dl>
+          <dl>
+            <dt>5月</dt>
+            <dd>1.1円</dd>
+          </dl>
+          <dl>
+            <dt>6月</dt>
+            <dd>1.1円</dd>
+          </dl>
+          <dl>
+            <dt>7月</dt>
+            <dd>1.1円</dd>
+          </dl>
+          <dl>
+            <dt>8月</dt>
+            <dd>1.1円</dd>
+          </dl>
+          <dl>
+            <dt>9月</dt>
+            <dd>1.1円</dd>
+          </dl>
+          <dl>
+            <dt>10月</dt>
+            <dd>0円</dd>
+          </dl>
+          <dl>
+            <dt>11月</dt>
+            <dd>0円</dd>
+          </dl>
+          <dl>
+            <dt>12月</dt>
+            <dd>0円</dd>
+          </dl>
+          <dl>
+            <dt>1月</dt>
+            <dd>0円</dd>
+          </dl>
+          <dl>
+            <dt>2月</dt>
+            <dd>0円</dd>
+          </dl>
+          <dl>
+            <dt>3月</dt>
+            <dd>0円</dd>
+          </dl>
+        </div>
+
+        <ul class="list">
+          <li>電話番号1つ当たりの料金です。</li>
+          <li>ご利用開始翌月より請求いたします。</li>
+          <li>音声SIMをご契約のお客様全員にご負担いただきます。</li>
+          <li>データSIM 020番号（M2M等専用番号）は電話リレーサービス料の対象外です。</li>
+          <li>上記は消費税を含む料金です。</li>
+        </ul>
+        <p class="sub">
+          （※1）番号単価は電話リレーサービス支援機関が定めており、一般社団法人電気通信事業者協会から公表されています。<br />
+          （※2）番号単価の算定は年度ごとに行われます。次年度以降の請求額は決定次第ご案内いたします。
+        </p>
+      </div>
+    </section>
+
+    <section class="content">
+      <div class="wrapper">
+        <h2>請求方法</h2>
+        <p>毎月のご利用料金に合算し請求します。</p>
+      </div>
+    </section>
+
+    <section class="content">
+      <div class="wrapper">
+        <h2>関連するHP</h2>
+        <p>電話リレーサービスについての詳細や、制度そのものに関するお問い合わせは、以下のページをご参照ください。</p>
+        <ul class="list">
+          <li><a href="https://www.soumu.go.jp/menu_seisaku/ictseisaku/telephonerelay/index.html" target="_blank">総務省</a></li>
+          <li><a href="https://nftrs.or.jp/" target="_blank">一般財団法人日本財団電話リレーサービス</a></li>
+          <li><a href="https://www.tca.or.jp/telephonerelay_service_support/" target="_blank">一般社団法人電気通信事業者協会 電話リレーサービス支援業務</a></li>
+        </ul>
+      </div>
+    </section>
+
+
+
+
+      <footer>
+        <?php get_footer(); ?>
+      </footer>
+
+
+    </div><!-- containerここまで -->
+
+    <?php get_template_part('inc/chat'); ?>
+  </body>
