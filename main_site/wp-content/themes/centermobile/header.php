721d024c67ab95e7b6a5ea8ae67675c5e6aecb0a
Mouri <mouri@zeromobile.co.jp>
2023-01-11 18:57:23 +0900

    mod: Clarity追加とcss修正

--- main_site/wp-content/themes/centermobile/header.php
+++ main_site/wp-content/themes/centermobile/header.php
@@ -10,6 +10,14 @@
     gtag('config', 'G-JNWWF18E56');
     gtag('config', 'UA-207144783-1');
   </script>
+  <!-- Clarity -->
+  <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "fc1l9z6054");
+  </script>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="format-detection" content="telephone=no">
