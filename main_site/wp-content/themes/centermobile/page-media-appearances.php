81fefcbac1c0bf82cab01f567a9f03512a92879d
Mouri <mouri@zeromobile.co.jp>
2023-12-01 11:13:17 +0900

    ENGINEER-2846 掲載メディアへの追加

--- main_site/wp-content/themes/centermobile/page-media-appearances.php
+++ main_site/wp-content/themes/centermobile/page-media-appearances.php
@@ -1,75 +1,82 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
   <?php get_header(); ?>
 </head>
-  <body>
 
-<!--ここから共通部分-->
-<?php get_template_part('inc/header'); ?>
-<!--ここまで共通部分-->
+<body>
 
-    <!-- ここからコンテンツ -->
-    <div class="container">
-      <div class="img_media top_image2">
-        <div class="top_tlt">
-          <div class="top_tlt_inner">
-            <h1 class="company_h1">掲載メディア一覧</h1>
-            <p>Press and Media Appearances</p>
-          </div>
+  <!--ここから共通部分-->
+  <?php get_template_part('inc/header'); ?>
+  <!--ここまで共通部分-->
+
+  <!-- ここからコンテンツ -->
+  <div class="container">
+    <div class="img_media top_image2">
+      <div class="top_tlt">
+        <div class="top_tlt_inner">
+          <h1 class="company_h1">掲載メディア一覧</h1>
+          <p>Press and Media Appearances</p>
         </div>
       </div>
+    </div>
 
 
-      <section class="media_appearances">
-        <div class="wrapper">
-          <p class="lead">センターモバイルを<br class="sp_only">ご紹介いただきました</p>
-          <ul class="media_appearances_list">
-            <li>
-              <p class="name">いろはに投資</p>
-              <p class="title"><a href="https://www.bridge-salon.jp/toushi/compare-low-cost-sim/" target="_blank">【2023年最新】格安スマホ(SIM)を徹底比較！どこがいい？人気おすすめランキング・料金・端末も紹介</a></p>
-              <p class="url"><a href="https://www.bridge-salon.jp/toushi/compare-low-cost-sim/" target="_blank">https://www.bridge-salon.jp/toushi/compare-low-cost-sim/</a></p>
-            </li>
-            <li>
-              <p class="name">ロケホン</p>
-              <p class="title"><a href="https://rokemoba.com/smartphone/centermobile-review/" target="_blank">センターモバイルの口コミ・評判からわかるメリット・デメリット！月額0円運用も可能？</a></p>
-              <p class="url"><a href="https://rokemoba.com/smartphone/centermobile-review/" target="_blank">https://rokemoba.com/smartphone/centermobile-review/</a></p>
-            </li>
-            <li>
-              <p class="name">日本ワーキングホリデー＆留学</p>
-              <p class="title"><a href="https://japanlifesupport.com/internet/sim.html" target="_blank">外国人向けの格安SIMカード</a></p>
-              <p class="url"><a href="https://japanlifesupport.com/internet/sim.html" target="_blank">https://japanlifesupport.com/internet/sim.html</a></p>
-            </li>
-            <li>
-              <p class="name">インタレスト・ウォッチ</p>
-              <p class="title"><a href="https://www.interest-watching.com/21033" target="_blank">センターモバイルの格安SIMなら基本料金0円も！口コミや評判は？</a></p>
-              <p class="url"><a href="https://www.interest-watching.com/21033" target="_blank">https://www.interest-watching.com/21033</a></p>
-            </li>
-            <li>
-              <p class="name">WATCH</p>
-              <p class="title"><a href="https://www.watching-review.com/323/" target="_blank">センターモバイルの格安SIMは料金が安いし無料も！口コミや評判は？</a></p>
-              <p class="url"><a href="https://www.watching-review.com/323/" target="_blank">https://www.watching-review.com/323/</a></p>
-            </li>
-            <li>
-              <p class="name">ザ・シネマ インターネット回線ニュース</p>
-              <p class="title"><a href="https://www.thecinema.jp/net/center-mobile-review/" target="_blank">センターモバイルの口コミ評判は実際どう？メリットデメリットまとめ</a></p>
-              <p class="url"><a href="https://www.thecinema.jp/net/center-mobile-review/" target="_blank">https://www.thecinema.jp/net/center-mobile-review/</a></p>
-            </li>
-            <li>
-              <p class="name">格安SIM劇場</p>
-              <p class="title"><a href="https://www.fami-geki.com/sim/centermobile/" target="_blank">センターモバイルの口コミ評判とは？月額0円の仕組みや料金・解約について解説</a></p>
-              <p class="url"><a href="https://www.fami-geki.com/sim/centermobile/" target="_blank">https://www.fami-geki.com/sim/centermobile/</a></p>
-            </li>
-          </ul>
-        </div>
-      </section>
+    <section class="media_appearances">
+      <div class="wrapper">
+        <p class="lead">センターモバイルを<br class="sp_only">ご紹介いただきました</p>
+        <ul class="media_appearances_list">
+          <li>
+            <p class="name">ぴかまろ</p>
+            <p class="title"><a href="https://iot-consulting.co.jp/pikamaro/erabikata/netosusume/mobilerooterosusume/wimax" target="_blank">おすすめWiMAXプロバイダ厳選8選を徹底比較！</a></p>
+            <p class="url"><a href="https://iot-consulting.co.jp/pikamaro/erabikata/netosusume/mobilerooterosusume/wimax" target="_blank">https://iot-consulting.co.jp/pikamaro/erabikata/netosusume/mobilerooterosusume/wimax</a></p>
+          </li>
+          <li>
+            <p class="name">いろはに投資</p>
+            <p class="title"><a href="https://www.bridge-salon.jp/toushi/compare-low-cost-sim/" target="_blank">【2023年最新】格安スマホ(SIM)を徹底比較！どこがいい？人気おすすめランキング・料金・端末も紹介</a></p>
+            <p class="url"><a href="https://www.bridge-salon.jp/toushi/compare-low-cost-sim/" target="_blank">https://www.bridge-salon.jp/toushi/compare-low-cost-sim/</a></p>
+          </li>
+          <li>
+            <p class="name">ロケホン</p>
+            <p class="title"><a href="https://rokemoba.com/smartphone/centermobile-review/" target="_blank">センターモバイルの口コミ・評判からわかるメリット・デメリット！月額0円運用も可能？</a></p>
+            <p class="url"><a href="https://rokemoba.com/smartphone/centermobile-review/" target="_blank">https://rokemoba.com/smartphone/centermobile-review/</a></p>
+          </li>
+          <li>
+            <p class="name">日本ワーキングホリデー＆留学</p>
+            <p class="title"><a href="https://japanlifesupport.com/internet/sim.html" target="_blank">外国人向けの格安SIMカード</a></p>
+            <p class="url"><a href="https://japanlifesupport.com/internet/sim.html" target="_blank">https://japanlifesupport.com/internet/sim.html</a></p>
+          </li>
+          <li>
+            <p class="name">インタレスト・ウォッチ</p>
+            <p class="title"><a href="https://www.interest-watching.com/21033" target="_blank">センターモバイルの格安SIMなら基本料金0円も！口コミや評判は？</a></p>
+            <p class="url"><a href="https://www.interest-watching.com/21033" target="_blank">https://www.interest-watching.com/21033</a></p>
+          </li>
+          <li>
+            <p class="name">WATCH</p>
+            <p class="title"><a href="https://www.watching-review.com/323/" target="_blank">センターモバイルの格安SIMは料金が安いし無料も！口コミや評判は？</a></p>
+            <p class="url"><a href="https://www.watching-review.com/323/" target="_blank">https://www.watching-review.com/323/</a></p>
+          </li>
+          <li>
+            <p class="name">センターモバイル インターネット回線ニュース</p>
+            <p class="title"><a href="https://centermobile.co.jp/net/center-mobile-review/" target="_blank">センターモバイルの口コミ評判は実際どう？メリットデメリットまとめ</a></p>
+            <p class="url"><a href="https://centermobile.co.jp/net/center-mobile-review/" target="_blank">https://centermobile.co.jp/net/center-mobile-review/</a></p>
+          </li>
+          <li>
+            <p class="name">格安SIM劇場</p>
+            <p class="title"><a href="https://www.fami-geki.com/sim/centermobile/" target="_blank">センターモバイルの口コミ評判とは？月額0円の仕組みや料金・解約について解説</a></p>
+            <p class="url"><a href="https://www.fami-geki.com/sim/centermobile/" target="_blank">https://www.fami-geki.com/sim/centermobile/</a></p>
+          </li>
+        </ul>
+      </div>
+    </section>
 
-      <footer>
-        <?php get_footer(); ?>
-      </footer>
+    <footer>
+      <?php get_footer(); ?>
+    </footer>
 
 
-    </div><!-- containerここまで -->
+  </div><!-- containerここまで -->
 
-    <?php get_template_part('inc/chat'); ?>
-  </body>
+  <?php get_template_part('inc/chat'); ?>
+</body>
