55aca92d6a5435737ef061f9dcb3334ca4212f7d
Mouri <mouri@zeromobile.co.jp>
2024-01-30 11:22:32 +0900

    ENGINEER-3337 外部送信ポリシーの修正

--- main_site/wp-content/themes/centermobile/page-cookie-policy.php
+++ main_site/wp-content/themes/centermobile/page-cookie-policy.php
@@ -1,151 +1,166 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
   <?php get_header(); ?>
 </head>
-  <body>
 
-<!--ここから共通部分-->
-<?php get_template_part('inc/header'); ?>
-<!--ここまで共通部分-->
+<body>
 
-      <!-- ここからコンテンツ -->
-    <div class="container">
-      <div class="img_policy top_image2">
-        <div class="top_tlt">
+  <!--ここから共通部分-->
+  <?php get_template_part('inc/header'); ?>
+  <!--ここまで共通部分-->
+
+  <!-- ここからコンテンツ -->
+  <div class="container">
+    <div class="img_policy top_image2">
+      <div class="top_tlt">
         <div class="top_tlt_inner">
-        <h1 class="company_h1">外部送信ポリシー</h1>
-        <p>Data Transfer Policy</p>
-        </div>
+          <h1 class="company_h1">外部送信ポリシー</h1>
+          <p>Data Transfer Policy</p>
         </div>
       </div>
+    </div>
 
 
     <section class="policy">
       <div class="wrapper">
-      <div>
-        <p class="mb-20">株式会社センターモバイル(以下、「当社」といいます)では、当社サービスを提供するにあたって、ご利用者様(当社サービスのユーザーの皆様)の当社ウェブサイトの閲覧や当社提供のアプリの利用を通して、ご利用者様に関する情報を、当社または当社が提携する事業者(以下、「送信先事業者」といいます)に送信しています(以下、「外部送信」といいます)。この外部送信ポリシー(以下、「当ポリシー」といいます)は、ご利用者様に関するどのような情報が、どこに、どのような目的で送信されるのかを、公表することを目的としています。</p>
-        <p class="mb-20">尚、外部送信した情報は、当ポリシーおよび送信先事業者のプライバシーポリシー等に基づいて管理・利用されています。尚、情報の外部送信を停止されたい場合は、以下に記載のとおり、各社のオプトアウトページや、利用者様のブラウザやスマートフォンなどの設定 を通じて停止することが可能です。</p>
-        <p class="mb-20">※ 個人情報保護法に定める個人情報(例えば、氏名、氏名とメールアドレスやクレジットカード番号の組み合わせなど、特定の個人を識別できる情報)に該当する情報の取り扱いについては、当社プライバシーポリシー(<a href="<?php echo esc_url( home_url( '/' ) ); ?>policy/"><?php echo esc_url( home_url( '/' ) ); ?>policy/</a>)にて定めていますので、そちらをご覧ください。</p>
-        <p class="mb-20">※ 当ページは、2023年6月16日施行の、改正電気通信事業法に基づく外部送信規律(電気通信事業法第27条の12、電気通信事業法施行規則第22条の2の28)における、ご利用者様の情報の外部送信における「確認付与の機会の提供」(利用者様情報の外部送信に関して、容易に知り得る状態に置くこと(公表))に対応するものです。</p>
+        <div>
+          <p class="mb-20">株式会社センターモバイル(以下、「当社」といいます)では、当社サービスを提供するにあたって、ご利用者様(当社サービスのユーザーの皆様)の当社ウェブサイトの閲覧や当社提供のアプリの利用を通して、ご利用者様に関する情報を、当社または当社が提携する事業者(以下、「送信先事業者」といいます)に送信しています(以下、「外部送信」といいます)。この外部送信ポリシー(以下、「当ポリシー」といいます)は、ご利用者様に関するどのような情報が、どこに、どのような目的で送信されるのかを、公表することを目的としています。</p>
+          <p class="mb-20">尚、外部送信した情報は、当ポリシーおよび送信先事業者のプライバシーポリシー等に基づいて管理・利用されています。尚、情報の外部送信を停止されたい場合は、以下に記載のとおり、各社のオプトアウトページや、利用者様のブラウザやスマートフォンなどの設定 を通じて停止することが可能です。</p>
+          <p class="mb-20">※ 個人情報保護法に定める個人情報(例えば、氏名、氏名とメールアドレスやクレジットカード番号の組み合わせなど、特定の個人を識別できる情報)に該当する情報の取り扱いについては、当社プライバシーポリシー(<a href="<?php echo esc_url(home_url('/')); ?>policy/"><?php echo esc_url(home_url('/')); ?>policy/</a>)にて定めていますので、そちらをご覧ください。</p>
+          <p class="mb-20">※ 当ページは、2023年6月16日施行の、改正電気通信事業法に基づく外部送信規律(電気通信事業法第27条の12、電気通信事業法施行規則第22条の2の28)における、ご利用者様の情報の外部送信における「確認付与の機会の提供」(利用者様情報の外部送信に関して、容易に知り得る状態に置くこと(公表))に対応するものです。</p>
 
-        <h3>本ポリシーに記載している内容</h3>
-        <ol>
-          <li><a href="#anchor1">外部送信する情報の利用目的</a></li>
-          <li><a href="#anchor2">外部送信する情報</a></li>
-          <li><a href="#anchor3">送信先事業者別の詳細(調査対象)</a></li>
-          <li><a href="#anchor4">外部送信の停止方法</a></li>
-          <li><a href="#anchor5">お問合せ先</a></li>
-          <li><a href="#anchor6">改定履歴</a></li>
-        </ol>
-      </div>
+          <h3>本ポリシーに記載している内容</h3>
+          <ol>
+            <li><a href="#anchor1">外部送信する情報の利用目的</a></li>
+            <li><a href="#anchor2">外部送信する情報</a></li>
+            <li><a href="#anchor3">送信先事業者別の詳細(調査対象)</a></li>
+            <li><a href="#anchor4">外部送信の停止方法</a></li>
+            <li><a href="#anchor5">お問合せ先</a></li>
+            <li><a href="#anchor6">改定履歴</a></li>
+          </ol>
+        </div>
 
-      <h2 id="anchor1">1. 外部送信する情報の利用目的</h2>
-      <div>
-        <p class="mb-20">外部送信する情報の利用目的には以下の種類があります(送信する情報の当社における利用目的及び送信先事業者における利用目的は送信先事業者ごとに異なります。「送信先事業者別の詳細」をご確認ください。)</p>
-        <p>(1) 広告、各種情報の配信</p>
-        <p>1) 当社の広告配信<br>
-        当社及び他社のウェブサイトやアプリ上で当社の広告を表示するため。</p>
-        <p>2) 当社の広告事業としての広告掲載・配信<br>
-        当社が運営するウェブサイトやアプリ上の広告枠に広告を掲載するため。</p>
-        <p>3) 他社の広告事業としての広告掲載・配信<br>
-        提携している他社とデータの連携をし、他社が、当社が運営するウェブサイトやアプリ上の広告枠に広告配信・掲載するため。</p>
-        <p class="mb-20">4) ウェブサイトやアプリの利便性を向上させるため<br>
-        当社が運営するウェブサイトやアプリでの利用者様の閲覧履歴などの情報をもとに、最適なコンテンツを表示するため。</p>
+        <h2 id="anchor1">1. 外部送信する情報の利用目的</h2>
+        <div>
+          <p class="mb-20">外部送信する情報の利用目的には以下の種類があります(送信する情報の当社における利用目的及び送信先事業者における利用目的は送信先事業者ごとに異なります。「送信先事業者別の詳細」をご確認ください。)</p>
+          <p>(1) 広告、各種情報の配信</p>
+          <p>1) 当社の広告配信<br>
+            当社及び他社のウェブサイトやアプリ上で当社の広告を表示するため。</p>
+          <p>2) 当社の広告事業としての広告掲載・配信<br>
+            当社が運営するウェブサイトやアプリ上の広告枠に広告を掲載するため。</p>
+          <p>3) 他社の広告事業としての広告掲載・配信<br>
+            提携している他社とデータの連携をし、他社が、当社が運営するウェブサイトやアプリ上の広告枠に広告配信・掲載するため。</p>
+          <p class="mb-20">4) ウェブサイトやアプリの利便性を向上させるため<br>
+            当社が運営するウェブサイトやアプリでの利用者様の閲覧履歴などの情報をもとに、最適なコンテンツを表示するため。</p>
 
-        <p>(2) 分析</p>
-        <p>1) 利用者様による閲覧の傾向や履歴分析<br>
-        当社が運営するウェブサイトやアプリ上での利用者様の行動履歴情報を収集し、その傾向などを分析するため。</p>
-        <p>2) 広告の効果分析<br>
-        当社が運営するウェブサイトやアプリ上の広告に対して利用者様が閲覧した情報、クリックした情報、およびウェブサイトやアプリ上での行動履歴情報を収集し、広告の効果を確認・計測・分析するため。</p>
-        <p class="mb-20">3) 不正や異常の監視<br>
-        当社が運営するウェブサイトやアプリ上の広告に関する不正または異常な閲覧数やクリック数などを検知し、適正に広告の効果を確認・計測・分析するため。</p>
+          <p>(2) 分析</p>
+          <p>1) 利用者様による閲覧の傾向や履歴分析<br>
+            当社が運営するウェブサイトやアプリ上での利用者様の行動履歴情報を収集し、その傾向などを分析するため。</p>
+          <p>2) 広告の効果分析<br>
+            当社が運営するウェブサイトやアプリ上の広告に対して利用者様が閲覧した情報、クリックした情報、およびウェブサイトやアプリ上での行動履歴情報を収集し、広告の効果を確認・計測・分析するため。</p>
+          <p class="mb-20">3) 不正や異常の監視<br>
+            当社が運営するウェブサイトやアプリ上の広告に関する不正または異常な閲覧数やクリック数などを検知し、適正に広告の効果を確認・計測・分析するため。</p>
 
-        <p>(3) 外部サービス連携</p>
-        <p>1) SNS との連携<br>
-        当社が運営するウェブサイトやアプリの利用者様による SNS を通じた情報共有などを実現 するため。</p>
-        <p>2) 外部送信プログラムの管理<br>
-        広告や分析を目的とした外部送信プログラムを管理するツールを利用するため。</p>
-      </div>
+          <p>(3) 外部サービス連携</p>
+          <p>1) SNS との連携<br>
+            当社が運営するウェブサイトやアプリの利用者様による SNS を通じた情報共有などを実現 するため。</p>
+          <p>2) 外部送信プログラムの管理<br>
+            広告や分析を目的とした外部送信プログラムを管理するツールを利用するため。</p>
+        </div>
 
-      <h2 id="anchor2">2. 外部送信する情報</h2>
-      <div>
-        <p>外部送信を行なう主な情報には以下の種類があります。(外部送信する情報は送信先事業者ごとに異なります。「送信先事業者別の詳細」をご確認ください。)</p>
-        <ol>
-          <li>閲覧した内容についての情報(カテゴリー分類、商品名等)</li>
-          <li>閲覧の履歴(閲覧した日時、URL、リンク元の URL等)</li>
-          <li>購入や行動の履歴(購入や申し込みの際の日時、Cookieの識別番号等)</li>
-          <li>閲覧した人や機器を識別する情報(ユーザーID、ブラウザ・デバイス識別子等)</li>
-          <li>閲覧した機器の位置についての情報(IPアドレス、GPS情報等)</li>
-        </ol>
-        <p>※ Cookieとは、Webサイトを閲覧する際にお客様が訪れたWebサイトの履歴や情報が記録されます。取得された情報は、当社の広告配信や様々なサービス改善に役立ちます。</p>
-      </div>
+        <h2 id="anchor2">2. 外部送信する情報</h2>
+        <div>
+          <p>外部送信を行なう主な情報には以下の種類があります。(外部送信する情報は送信先事業者ごとに異なります。「送信先事業者別の詳細」をご確認ください。)</p>
+          <ol>
+            <li>閲覧した内容についての情報(カテゴリー分類、商品名等)</li>
+            <li>閲覧の履歴(閲覧した日時、URL、リンク元の URL等)</li>
+            <li>購入や行動の履歴(購入や申し込みの際の日時、Cookieの識別番号等)</li>
+            <li>閲覧した人や機器を識別する情報(ユーザーID、ブラウザ・デバイス識別子等)</li>
+            <li>閲覧した機器の位置についての情報(IPアドレス、GPS情報等)</li>
+          </ol>
+          <p>※ Cookieとは、Webサイトを閲覧する際にお客様が訪れたWebサイトの履歴や情報が記録されます。取得された情報は、当社の広告配信や様々なサービス改善に役立ちます。</p>
+        </div>
 
-      <h2 id="anchor3">3. 送信先事業者別の詳細(調査対象)</h2>
-      <div>
-        <p class="mb-20">当社サービスを利用される利用者様に関する情報を外部送信している外部のサービスは以下のとおりです。<br>
-        オプトアウトページの記載があるものは、リンク先のページで、情報の外部送信の停止や、送信先事業者での利用停止手続きが可能です。</p>
-        <p>最終調査月: 2023年6月現在</p>
-        <p class="mb-20">調査機関: 株式会社センターモバイル</p>
+        <h2 id="anchor3">3. 送信先事業者別の詳細(調査対象)</h2>
+        <div>
+          <p class="mb-20">当社サービスを利用される利用者様に関する情報を外部送信している外部のサービスは以下のとおりです。<br>
+            オプトアウトページの記載があるものは、リンク先のページで、情報の外部送信の停止や、送信先事業者での利用停止手続きが可能です。</p>
+          <p>最終調査月: 2023年6月現在</p>
+          <p class="mb-20">調査機関: 株式会社センターモバイル</p>
 
-        <p>【Google アナリティクスの利用】</p>
-        <p>送信先事業者名: Google LLC</p>
-        <p class="mb-20">Webサイト: <a href="https://analytics.google.com/analytics/web/provision/#/provision" target="_blank">https://analytics.google.com/analytics/web/provision/#/provision</a></p>
+          <p>【Google アナリティクスの利用】</p>
+          <p>送信先事業者名: Google LLC</p>
+          <p class="mb-20">Webサイト: <a href="https://analytics.google.com/analytics/web/provision/#/provision" target="_blank">https://analytics.google.com/analytics/web/provision/#/provision</a></p>
 
-        <p>【送信先事業者における利用目的】</p>
-        <p>以下の送信先事業者のプライバシーポリシーに記載のとおり</p>
-        <p>プライバシーポリシー: <a href="https://policies.google.com/privacy" target="_blank">https://policies.google.com/privacy</a></p>
-        <p class="mb-20">「Google のサービスを使用するサイトやアプリから収集した情報の Google による使用」: <a href="https://policies.google.com/technologies/partner-sites" target="_blank">https://policies.google.com/technologies/partner-sites</a></p>
+          <p>【送信先事業者における利用目的】</p>
+          <p>以下の送信先事業者のプライバシーポリシーに記載のとおり</p>
+          <p>プライバシーポリシー: <a href="https://policies.google.com/privacy" target="_blank">https://policies.google.com/privacy</a></p>
+          <p class="mb-20">「Google のサービスを使用するサイトやアプリから収集した情報の Google による使用」: <a href="https://policies.google.com/technologies/partner-sites" target="_blank">https://policies.google.com/technologies/partner-sites</a></p>
 
-        <p>【オプトアウトページ】</p>
-        <p class="mb-20"><a href="https://tools.google.com/dlpage/gaoptout/" target="_blank">https://tools.google.com/dlpage/gaoptout/</a></p>
+          <p>【オプトアウトページ】</p>
+          <p class="mb-20"><a href="https://tools.google.com/dlpage/gaoptout/" target="_blank">https://tools.google.com/dlpage/gaoptout/</a></p>
 
-        <p>【広告配信を目的としたタグの利用】</p>
-        <p>広告名：AppLovin</p>
-        <p>提携企業：AppLovin corporation</p>
-        <p class="mb-20">プライバシーポリシー：<a href="https://www.applovin.com/privacy-jp/" target="_blank">https://www.applovin.com/privacy-jp/</a></p>
+          <p>【広告配信を目的としたタグの利用】</p>
+          <p>広告名：AppLovin</p>
+          <p>提携企業：AppLovin corporation</p>
+          <p class="mb-20">プライバシーポリシー：<a href="https://www.applovin.com/privacy-jp/" target="_blank">https://www.applovin.com/privacy-jp/</a></p>
 
-        <p>広告名：AdMob</p>
-        <p>提携企業：Google LLC</p>
-        <p class="mb-20">プライバシーポリシー：<a href="https://policies.google.com/privacy?hl=ja" target="_blank">https://policies.google.com/privacy?hl=ja</a></p>
+          <p>広告名：AdMob</p>
+          <p>提携企業：Google LLC</p>
+          <p class="mb-20">プライバシーポリシー：<a href="https://policies.google.com/privacy?hl=ja" target="_blank">https://policies.google.com/privacy?hl=ja</a></p>
 
-        <p>広告名：SKYFLAG</p>
-        <p>提携企業：株式会社Skyfall</p>
-        <p class="mb-20">プライバシーポリシー：<a href="https://skyfall.co.jp/privacy-policy/" target="_blank">https://skyfall.co.jp/privacy-policy/</a></p>
+          <?php
+          date_default_timezone_set('Asia/Tokyo');
+          if (strtotime(date('Y-m-d H:i')) < strtotime('2024-02-01 00:00')) :
+          ?>
+            <p>広告名：SKYFLAG</p>
+            <p>提携企業：株式会社Skyfall</p>
+            <p class="mb-20">プライバシーポリシー：<a href="https://skyfall.co.jp/privacy-policy/" target="_blank">https://skyfall.co.jp/privacy-policy/</a></p>
+          <?php else : ?>
+            <p>広告名：AppDriver</p>
+            <p>提携企業：株式会社アドウェイズ</p>
+            <p class="mb-20">プライバシーポリシー：<a href="https://www.adways.net/privacy.html" target="_blank">https://www.adways.net/privacy.html</a></p>
+          <?php endif; ?>
 
-        <p>※ 定期的に調査を行い本ポリシーへ反映を行っておりますが、Webサイト・アプリの更新タイミングにより、記載内容と実態との差が発生する場合がございます。予めご了承ください。</p>
-      </div>
+          <p>※ 定期的に調査を行い本ポリシーへ反映を行っておりますが、Webサイト・アプリの更新タイミングにより、記載内容と実態との差が発生する場合がございます。予めご了承ください。</p>
+        </div>
 
-      <h2 id="anchor4">4. 外部送信の停止方法</h2>
-      <div>
-        <p class="mb-20">外部送信については、以下の方法で停止することが可能な場合があります。ただし、外部送信の停止手続きを行なうことによって、Webサイトやアプリの通常の利用において支障をきたす場合があるため、停止手続きを行うにあたってはご注意ください。</p>
-        <p>(1) 送信先のオプトアウトページで停止する方法<br>
-        上記 3.送信先事業者別の詳細情報に記載のオプトアウトページをご確認ください。</p>
-        <p>(2) Web ブラウザで設定を行なう方法</p>
-        <ul>
-          <li>safari の場合:<br>
-          <a href="https://support.apple.com/ja-jp/HT212025" target="_blank">https://support.apple.com/ja-jp/HT212025</a></li>
-          <li>MicroSoft Edge の場合:<br>
-          <a href="https://learn.microsoft.com/ja-jp/microsoft-edge/privacy-whitepaper/#do-not-track" target="_blank">https://learn.microsoft.com/ja-jp/microsoft-edge/privacy-whitepaper/#do-not-track</a></li>
-          <li>Google Chrome の場合:<br>
-          <a href="https://support.google.com/chrome/answer/2790761?hl=ja&co=GENIE.Platform%3DDesktop" target="_blank">https://support.google.com/chrome/answer/2790761?hl=ja&co=GENIE.Platform%3DDesktop</a></li>
-          <li>Fire Fox の場合:<br>
-          <a href="https://support.mozilla.org/ja/kb/how-do-i-turn-do-not-track-feature" target="_blank">https://support.mozilla.org/ja/kb/how-do-i-turn-do-not-track-feature</a></li>
-        </ul>
-      </div>
+        <h2 id="anchor4">4. 外部送信の停止方法</h2>
+        <div>
+          <p class="mb-20">外部送信については、以下の方法で停止することが可能な場合があります。ただし、外部送信の停止手続きを行なうことによって、Webサイトやアプリの通常の利用において支障をきたす場合があるため、停止手続きを行うにあたってはご注意ください。</p>
+          <p>(1) 送信先のオプトアウトページで停止する方法<br>
+            上記 3.送信先事業者別の詳細情報に記載のオプトアウトページをご確認ください。</p>
+          <p>(2) Web ブラウザで設定を行なう方法</p>
+          <ul>
+            <li>safari の場合:<br>
+              <a href="https://support.apple.com/ja-jp/HT212025" target="_blank">https://support.apple.com/ja-jp/HT212025</a>
+            </li>
+            <li>MicroSoft Edge の場合:<br>
+              <a href="https://learn.microsoft.com/ja-jp/microsoft-edge/privacy-whitepaper/#do-not-track" target="_blank">https://learn.microsoft.com/ja-jp/microsoft-edge/privacy-whitepaper/#do-not-track</a>
+            </li>
+            <li>Google Chrome の場合:<br>
+              <a href="https://support.google.com/chrome/answer/2790761?hl=ja&co=GENIE.Platform%3DDesktop" target="_blank">https://support.google.com/chrome/answer/2790761?hl=ja&co=GENIE.Platform%3DDesktop</a>
+            </li>
+            <li>Fire Fox の場合:<br>
+              <a href="https://support.mozilla.org/ja/kb/how-do-i-turn-do-not-track-feature" target="_blank">https://support.mozilla.org/ja/kb/how-do-i-turn-do-not-track-feature</a>
+            </li>
+          </ul>
+        </div>
 
-      <h2 id="anchor5">5. お問い合わせ先</h2>
-      <div>
-        <p>外部送信の情報の取扱いに関するお問い合わせは、以下にご連絡ください。</p>
-        <p><a href="mailto:&#108;&#101;&#103;&#97;&#108;&#64;&#99;&#101;&#110;&#116;&#101;&#114;&#109;&#111;&#98;&#105;&#108;&#101;&#46;&#99;&#111;&#46;&#106;&#112;">&#108;&#101;&#103;&#97;&#108;&#64;&#99;&#101;&#110;&#116;&#101;&#114;&#109;&#111;&#98;&#105;&#108;&#101;&#46;&#99;&#111;&#46;&#106;&#112;</a></p>
-      </div>
+        <h2 id="anchor5">5. お問い合わせ先</h2>
+        <div>
+          <p>外部送信の情報の取扱いに関するお問い合わせは、以下にご連絡ください。</p>
+          <p><a href="mailto:&#108;&#101;&#103;&#97;&#108;&#64;&#99;&#101;&#110;&#116;&#101;&#114;&#109;&#111;&#98;&#105;&#108;&#101;&#46;&#99;&#111;&#46;&#106;&#112;">&#108;&#101;&#103;&#97;&#108;&#64;&#99;&#101;&#110;&#116;&#101;&#114;&#109;&#111;&#98;&#105;&#108;&#101;&#46;&#99;&#111;&#46;&#106;&#112;</a></p>
+        </div>
 
-      <h2 id="anchor6">6. 改定履歴</h2>
-      <div>
-        <p>当社サービスにおける外部送信の定期的に調査するとともに、必要に応じて当ポリシーの改定を行います。尚、当ポリシーは、改定後の内容が掲載された時点で更新された内容が即時に有効となります。</p>
-      </div>
+        <h2 id="anchor6">6. 改定履歴</h2>
+        <div>
+          <p>当社サービスにおける外部送信の定期的に調査するとともに、必要に応じて当ポリシーの改定を行います。尚、当ポリシーは、改定後の内容が掲載された時点で更新された内容が即時に有効となります。</p>
+        </div>
 
-      <div class="date">2023年6月16日制定</div>
+        <div class="date">2023年6月16日制定</div>
 
     </section>
 
@@ -154,12 +169,12 @@
 
 
 
-      <footer>
-        <?php get_footer(); ?>
-      </footer>
+    <footer>
+      <?php get_footer(); ?>
+    </footer>
 
 
-    </div><!-- containerここまで -->
+  </div><!-- containerここまで -->
 
-    <?php get_template_part('inc/chat'); ?>
-  </body>
+  <?php get_template_part('inc/chat'); ?>
+</body>
