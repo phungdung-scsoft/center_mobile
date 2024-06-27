22aafa310e11ba6771966629bde962c17df34a0f
Mouri <mouri@zeromobile.co.jp>
2024-06-27 18:33:34 +0900

    トピックスインデント

--- main_site/wp-content/themes/centermobile/single.php
+++ main_site/wp-content/themes/centermobile/single.php
@@ -1,42 +1,44 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
   <?php get_header(); ?>
 </head>
-  <body>
 
-<!--ここから共通部分-->
-<?php get_template_part('inc/header'); ?>
-<!--ここまで共通部分-->
+<body>
 
-    <div class="container">
-      <div class="img_oem top_image2">
-        <div class="top_tlt">
+  <!--ここから共通部分-->
+  <?php get_template_part('inc/header'); ?>
+  <!--ここまで共通部分-->
+
+  <div class="container">
+    <div class="img_oem top_image2">
+      <div class="top_tlt">
         <div class="top_tlt_inner">
           <h1 class="company_h1">トピックス</h1>
           <p>Topics</p>
         </div>
-        </div>
       </div>
+    </div>
 
-      <section>
-        <div class="wrapper">
-          <div class="news_inner">
-            <h3><?php the_title(); ?></h3>
-            <div class="date"><?php echo get_the_date( $format, $post ); ?></div>
-            <div class="news_contents">
-              <?php the_content(); ?>
-            </div>
+    <section>
+      <div class="wrapper">
+        <div class="news_inner">
+          <h3><?php the_title(); ?></h3>
+          <div class="date"><?php echo get_the_date($format, $post); ?></div>
+          <div class="news_contents">
+            <?php the_content(); ?>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <footer>
-        <?php get_footer(); ?>
-      </footer>
+    <footer>
+      <?php get_footer(); ?>
+    </footer>
 
 
-    </div><!-- containerここまで -->
+  </div><!-- containerここまで -->
 
-    <?php get_template_part('inc/chat'); ?>
-  </body>
+  <?php get_template_part('inc/chat'); ?>
+</body>
