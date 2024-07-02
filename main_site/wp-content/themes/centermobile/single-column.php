e072d45dfe70035e7c8f78b987377611787980d6
Mouri <mouri@zeromobile.co.jp>
2024-07-02 16:37:09 +0900

    コラムページ更新

--- main_site/wp-content/themes/centermobile/single-column.php
+++ main_site/wp-content/themes/centermobile/single-column.php
@@ -46,10 +46,66 @@
           <div class="news_contents">
             <?php the_content(); ?>
           </div>
+
+          <!-- 関連記事のセクション -->
+          <div class="related-posts">
+            <div>
+              <h3>関連記事</h3>
+              <ul class="related-posts-list">
+                <?php
+                $the_id = get_the_ID();
+                $not_in = [$the_id];
+                $maxnum = 4;
+
+                $args = [
+                  'post__not_in'        => $not_in,
+                  'post_type'           => get_post_type(),
+                  'post_status'         => 'publish',
+                  'no_found_rows'       => true,
+                  'ignore_sticky_posts' => true,
+                  'orderby'             => 'rand',
+                  'posts_per_page'      => $maxnum,
+                ];
+
+                $categories = get_the_category($the_id);
+                $cat_array  = [];
+
+                foreach ($categories as $the_cat) {
+                  array_push($cat_array, $the_cat->cat_ID);
+                }
+                if (!empty($cat_array)) {
+                  $args['category__in'] = $cat_array;
+                }
+
+                $related_query = new WP_Query($args);
+
+                if (!$related_query->have_posts()) :
+                  echo '<li>関連記事がありません。</li>';
+                else :
+                  while ($related_query->have_posts()) : $related_query->the_post();
+                ?>
+                    <li class="related-post-item">
+                      <a href="<?php the_permalink(); ?>">
+                        <?php if (has_post_thumbnail()) : ?>
+                          <?php the_post_thumbnail('medium'); ?>
+                        <?php else : ?>
+                          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/default-thumbnail.png" alt="No Image">
+                        <?php endif; ?>
+                        <p class="related-post-title"><?php the_title(); ?></p>
+                        <p class="related-post-date"><?php echo get_the_date('Y年m月d日'); ?></p>
+                      </a>
+                    </li>
+                <?php
+                  endwhile;
+                  wp_reset_postdata();
+                endif;
+                ?>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </section>
-
     <footer>
       <?php get_footer(); ?>
     </footer>
@@ -59,3 +115,5 @@
 
   <?php /*get_template_part('inc/chat');*/ ?>
 </body>
+
+</html>
